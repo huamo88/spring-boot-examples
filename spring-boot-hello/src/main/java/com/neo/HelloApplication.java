6468d1725a0f12be01498aa073045b137a541ef9
huamo88 <371107787@qq.com>
2019-08-06 10:33:46 +0800

    提交代码

--- spring-boot-hello/src/main/java/com/neo/HelloApplication.java
+++ spring-boot-hello/src/main/java/com/neo/HelloApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApplication {
 
 	public static void main(String[] args) {
+		System.out.println("启动项目");
 		SpringApplication.run(HelloApplication.class, args);
 	}
 }
