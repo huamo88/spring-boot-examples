b7b909ce140851dab954a210b5f9a6876c8d6bef
huamo88 <371107787@qq.com>
2019-08-07 10:21:15 +0800

    11111

--- spring-boot-hello/src/main/java/com/neo/HelloApplication.java
+++ spring-boot-hello/src/main/java/com/neo/HelloApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApplication {
 
 	public static void main(String[] args) {
-		System.out.println("启动项目2019");
+		System.out.println("启动项目Spring2019");
 		SpringApplication.run(HelloApplication.class, args);
 	}
 }
