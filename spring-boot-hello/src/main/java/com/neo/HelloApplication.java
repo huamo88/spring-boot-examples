54adbd0a999332ef97df2e9dc4e49a9c6e199e53
huamo88 <371107787@qq.com>
2019-08-07 11:05:45 +0800

    test

--- spring-boot-hello/src/main/java/com/neo/HelloApplication.java
+++ spring-boot-hello/src/main/java/com/neo/HelloApplication.java
@@ -8,7 +8,7 @@ public class HelloApplication {
 
 	public static void main(String[] args) {
 		System.out.println("启动项目Spring");
-		System.out.println("启动项目Spring2019.08.07.10");
+		System.out.println("启动项目Spring2019.08.07");
 		SpringApplication.run(HelloApplication.class, args);
 	}
 }
